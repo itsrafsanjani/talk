cea01366309aea412df5df01916f75235c56f0b5
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-08-21 10:10:27 +0600

    fix: login and register route name used in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,8 +74,8 @@
             @if (Auth::check())
                 <a href="{{ route('home') }}">Home</a>
             @else
-                <a href="{{ route('/login') }}">Login</a>
-                <a href="{{ route('/register') }}">Register</a>
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
             @endif
         </div>
     @endif
